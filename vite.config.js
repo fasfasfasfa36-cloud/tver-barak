75f821a3b5f70dea5f17146e8ce122c7ce6636b6
fasfasfasfa36-cloud <fasfasfasfa36@gmail.com>
2026-01-23 18:37:35 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    hmr: {
-      overlay: false,  // ← отключает красные оверлеи ошибок в браузере
-    },
-  },
-})
+  base: './',  // Это делает все пути относительными
+});                                  
